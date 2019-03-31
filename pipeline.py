bf1cb39ead9a4a4c41f86b6611711e462b53ffca
aboga <aboga@MacBook-Pro-aboga.local>
2019-03-31 20:35:25 +0200

    Create tf dataset for binary classification. Pipeline validates car_damage_dataset against mobilenet_v2 NN

--- pipeline.py
+++ pipeline.py
@@ -23,8 +23,6 @@ label_to_index = dict((name, index) for index, name in enumerate(label_names))
 
 all_image_labels = [label_to_index[pathlib.Path(path).parent.name] for path in all_image_paths]
 
-print("First 10 labels indices: ", all_image_labels[:10])
-
 img_path = all_image_paths[0]
 img_raw = tf.read_file(img_path)
 
@@ -32,9 +30,6 @@ img_tensor = tf.image.decode_image(img_raw)
 img_final = tf.image.resize_images(img_tensor, [192, 192])
 img_final = img_final/255.0
 
-print(img_final.shape)
-print(img_final.numpy().min())
-print(img_final.numpy().max())
 
 def preprocess_image(image):
     image = tf.image.decode_jpeg(image, channels=3)
@@ -51,15 +46,69 @@ def load_and_preprocess_image(path):
 path_ds = tf.data.Dataset.from_tensor_slices(all_image_paths)
 image_ds = path_ds.map(load_and_preprocess_image, num_parallel_calls=AUTOTUNE)
 
-import matplotlib.pyplot as plt
+label_ds = tf.data.Dataset.from_tensor_slices(tf.cast(all_image_labels, tf.int64))
 
+for label in label_ds.take(10):
+  print(label_names[label.numpy()])
 
-plt.figure(figsize=(8,8))
-for n,image in enumerate(image_ds.take(4)):
-  plt.subplot(2,2,n+1)
-  plt.imshow(image)
-  plt.grid(False)
-  plt.xticks([])
-  plt.yticks([])
-  plt.xlabel("Image")
-  plt.show()
+image_label_ds = tf.data.Dataset.zip((image_ds, label_ds))
+
+print('image shape: ', image_label_ds.output_shapes[0])
+print('label shape: ', image_label_ds.output_shapes[1])
+print('types: ', image_label_ds.output_types)
+print()
+print(image_label_ds)
+
+ds = tf.data.Dataset.from_tensor_slices((all_image_paths, all_image_labels))
+
+def load_and_preprocess_from_path_label(path, label):
+  return load_and_preprocess_image(path), label
+
+image_label_ds = ds.map(load_and_preprocess_from_path_label) 
+
+BATCH_SIZE = 32
+
+ds = image_label_ds.shuffle(buffer_size=image_count)
+ds = ds.repeat()
+ds = ds.batch(BATCH_SIZE)
+
+ds.prefetch(buffer_size=AUTOTUNE)
+
+ds
+
+mobile_net = tf.keras.applications.MobileNetV2(input_shape=(192,192,3), include_top=False)
+mobile_net.trainable=False
+
+def change_range(image, label):
+  return 2*image-1, label
+
+keras_ds = ds.map(change_range)
+
+image_batch, label_batch = next(iter(keras_ds))
+
+feature_map_batch = mobile_net(image_batch)
+print(feature_map_batch.shape) 
+
+model = tf.keras.Sequential([
+  mobile_net,
+  tf.keras.layers.GlobalAveragePooling2D(),
+  tf.keras.layers.Dense(len(label_names))
+])
+
+logit_batch = model(image_batch).numpy()
+
+print("min logit: ", logit_batch.min())
+print("max logit: ", logit_batch.max())
+print()
+
+print('Shape: ', logit_batch.shape)
+
+model.compile(optimizer=tf.train.AdamOptimizer(),
+loss=tf.keras.losses.sparse_categorical_crossentropy,
+metrics=["accuracy"])
+
+model.summary()
+
+steps_per_epoch=tf.ceil(len(all_image_paths)/BATCH_SIZE).numpy()
+
+model.fit(ds, epochs=1, steps_per_epoch=int(steps_per_epoch))
