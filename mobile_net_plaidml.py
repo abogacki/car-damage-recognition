71ada94dcdc99872549face1f2d61e2eea3da645
aboga <aboga@MacBook-Pro-aboga.local>
2019-04-10 19:00:45 +0200

    Fixes sequential input size

--- mobile_net_plaidml.py
+++ mobile_net_plaidml.py
@@ -1,10 +1,16 @@
+# Only 2 lines will be added
+# Rest of the flow and code remains the same as default keras
+import plaidml.keras
+plaidml.keras.install_backend()
+
+import numpy as np
+import time
+from checkpoint import create_checkpoint_callback
+from load_data import load_dataset
+import keras
 import os
 os.environ["KERAS_BACKEND"] = "plaidml.keras.backend"
 
-import keras
-from load_data import load_dataset
-from checkpoint import create_checkpoint_callback
-import time
 
 DATA_PATH = '/Users/aboga/repos/car-damage-dataset/data2a/training'
 ds, image_count, label_names, steps_per_epoch = load_dataset(DATA_PATH, 32)
@@ -19,29 +25,38 @@ checkpoint_dir = os.path.dirname(checkpoint_path)
 cp_callback = create_checkpoint_callback(checkpoint_dir)
 
 model = keras.Sequential([
-    # keras.layers.Dense(300, input_shape=(192,192,3), activation='relu'),
-    # keras.layers.Dense(150, activation='relu'),
-    # keras.layers.GlobalAveragePooling2D(),
-    # keras.layers.Dense(len(label_names), activation='relu'),
-    keras.layers.Activation('softmax')
+    keras.layers.Conv2D(32, (3, 3), input_shape=(192, 192, 3)),
+    keras.layers.Activation('relu'),
+    keras.layers.MaxPooling2D(pool_size=(2, 2)),
+    keras.layers.Conv2D(32, (3, 3)),
+    keras.layers.Activation('relu'),
+    keras.layers.MaxPooling2D(pool_size=(2, 2)),
+    keras.layers.Conv2D(32, (3, 3)),
+    keras.layers.Activation('relu'),
+    keras.layers.MaxPooling2D(pool_size=(2, 2)),
+    keras.layers.Flatten(),
+    keras.layers.Dense(32),
+    keras.layers.Activation('relu'),
+    keras.layers.Dropout(0.5),
+    keras.layers.Dense(len(label_names)),
+    keras.layers.Activation('sigmoid')
 ])
+model.summary()
+quit()
 
-model.compile(optimizer='sgd',
+model.compile(optimizer='rmsprop',
               loss=keras.losses.sparse_categorical_crossentropy,
               metrics=["accuracy"])
 
-model.summary()
 
 # log results
 
 TEST_DATA_PATH = '/Users/aboga/repos/car-damage-dataset/data2a/validation'
-test_ds, test_image_count, test_label_names, test_steps_per_epoch = load_dataset(TEST_DATA_PATH, 32)
-
-import numpy as np
-
-model.fit(np.array(ds), epochs=1, steps_per_epoch=int(steps_per_epoch), callbacks=[cp_callback])
-
-model.evaluate(np.array(test_ds), batch_size=32, steps=30)
+test_ds, test_image_count, test_label_names, test_steps_per_epoch = load_dataset(
+    TEST_DATA_PATH, 32)
 
 
+model.fit(np.array(ds), epochs=1, steps_per_epoch=int(
+    steps_per_epoch), callbacks=[cp_callback], validation_data=np.array(test_ds))
 
+# model.evaluate(np.array(test_ds), batch_size=32, steps=30)
